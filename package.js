5961230097866b48a9dfe30fab465a5ab112d02e
Isaac Han <isaac@hi16.ca>
2017-01-28 23:11:08 -0700

    graph shows

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
     summary: 'Style-query: JQuerified stylesheet library.',
-    version: '0.0.12',
+    "version": "0.0.14",
     git: 'https://github.com/i4han/style-query.git',
     documentation: 'README.md'
 });
@@ -9,7 +9,7 @@ Package.describe({
 Package.on_use( function (api) {
     api.use('coffeescript@1.2.6');
     // api.use('jquery@1.0.1');
-    // api.use('isaac:absurd@0.1.2');
+    api.use('isaac:absurd@0.1.2');
     api.use('isaac:underscore2@0.5.40');
     api.add_files( 'style-query.coffee', 'client');
 });
